9302810e77f1f77e3a038c1aaa10db70fd46cbd4
Jon Tejada <hi@jontejada.com>
2016-03-21 23:33:02 -0700

    notes additions

--- readme.md
+++ readme.md
@@ -8,6 +8,13 @@
 2. if left > right, swap
 3. repeat 1, 2 until zero or one unsorted (leftmost) element remains
 
+**OR**
+
+- compare adjacent, swap if out of order
+- repeat until no swaps are needed
+- this bubbles the highest to the end
+- very slow, may be ok if elemens are mostly in order
+- best case of fully sorted is O(n)
 
 ##insertion sort
 - O(n^2)
@@ -15,11 +22,27 @@
 1. find the position that holds the minimum value in the unsorted sub-array
 2. swap the found minimum into its correct location (leftmost position of unsorted sub-array)
 
+OR
+
+- grab one element from the unsorted sub-array
+- shift the element into the sorted sub-array (shift back until in correct position)
+
 ##selection sort
 - O(n^2)
+- build sorted sub-array from left to right by finding minimum valued element in the unsorted sub-array
+- swap the min into place (filling left to right)
+
 
 ##quick sort
 - O(n*log(n))
+- pick a pivot
+- partition around the pivot (pivot element ends up in it's final position)
+- recursively repeat steps on left and right sub-arrays until they are all of size one or zero
+- concat back into one list
+
 
 ##merge sort
 - O(n*log(n))
+- divide the array into one-element sub-arrays
+- merge pairs by repeatedly grabbing least of the left values and inserting into new sub-array
+- repeat until a single (sorted) array is left 
--- sorts.js
+++ sorts.js
@@ -1,94 +1,94 @@
 var sorts = {},
-	arr1 = [5,6,3,2,1,4,0],
-	arr2 = [1,4,6,2,0,45,255];
+    arr1 = [5,6,3,2,1,4,0],
+    arr2 = [1,4,6,2,0,45,255];
 
 //BUBBLE SORT
 sorts.bubbleSort = function (arr){
-	for (var i = 0; i < arr.length; i++) {
-		for (var j = 0; j < arr.length - i - 1; j++) {
-			if(arr[j+1] < arr[j]) {
-				var temp = arr[j];
-				arr[j] = arr[j+1];
-				arr[j+1] = temp;
-			}
-		}
-	}
-	return arr;
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = 0; j < arr.length - i - 1; j++) {
+            if(arr[j+1] < arr[j]) {
+                var temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    return arr;
 };
 // console.log(sorts.bubbleSort(arr1));
 
 //SELECTION SORT
 sorts.selectionSort = function(arr) {
-	for (var i = 0; i < arr.length; i++) {
-		var minAt = i;
-		for (var j = i; j < arr.length; j++) {
-			if (arr[j] < arr[minAt]) {
-				minAt = j;
-			}
-		}
-		var temp = arr[i];
-		arr[i] = arr[minAt];
-		arr[minAt] = temp;
-	}
-	return arr;
+    for (var i = 0; i < arr.length; i++) {
+        var minAt = i;
+        for (var j = i; j < arr.length; j++) {
+            if (arr[j] < arr[minAt]) {
+                minAt = j;
+            }
+        }
+        var temp = arr[i];
+        arr[i] = arr[minAt];
+        arr[minAt] = temp;
+    }
+    return arr;
 };
 // console.log(sorts.selectionSort(arr1));
 
 //INSERTION SORT
 sorts.insertionSort = function(arr) {
-	for (var i = 0; i < arr.length; i++) {
-		var right = i + 1;
-		var left = i;
-		while (arr[right] < arr[left] && left >= 0) {
-			var temp = arr[right];
-			arr[right] = arr[left];
-			arr[left] = temp;
-			left --;
-			right--;
-		}
-	}
-	return arr;
+    for (var i = 0; i < arr.length; i++) {
+        var right = i + 1;
+        var left = i;
+        while (arr[right] < arr[left] && left >= 0) {
+            var temp = arr[right];
+            arr[right] = arr[left];
+            arr[left] = temp;
+            left --;
+            right--;
+        }
+    }
+    return arr;
 };
 // console.log(sorts.insertionSort(arr1));
 
 //QUICK SORT
 // sorts.quickSort = function(arr,left,right) {
 function quickSort(arr,left,right) {
-	if (right > left) {
-		// console.log(arr);
-		var index = left;
-		var pivot = arr[index];
-		arr = partition(arr, pivot);
-		quickSort(arr,left, index - 1);
-		quickSort(arr,index + 1, right);
-	}
-	return arr;
+    if (right > left) {
+        // console.log(arr);
+        var index = left;
+        var pivot = arr[index];
+        arr = partition(arr, pivot);
+        quickSort(arr,left, index - 1);
+        quickSort(arr,index + 1, right);
+    }
+    return arr;
 }
 console.log(quickSort(arr1,0,arr1.length));
 // console.log(quickSort(arr1));
 
 function swap(arr, first, second) {
-	var temp = arr[first];
-	arr[first] = arr[second];
-	arr[second] = temp;
+    var temp = arr[first];
+    arr[first] = arr[second];
+    arr[second] = temp;
 }
 
 function partition(arr,pivot) {
-	var i = 0;
-	for (var j = 0; j < arr.length; j++) {
-		if (arr[j]!= pivot && arr[j] < pivot) {
-			swap(arr,i,j);
-			i++;
-		}
-	}
-	return arr;
+    var i = 0;
+    for (var j = 0; j < arr.length; j++) {
+        if (arr[j]!= pivot && arr[j] < pivot) {
+            swap(arr,i,j);
+            i++;
+        }
+    }
+    return arr;
 }
 
 
 //MERGE SORT
 sorts.mergeSort = function(arr) {
 
-	return arr;
+    return arr;
 };
 // console.log(sorts.mergeSort(arr1));
 
